621dc171c16e61511b71bcd939b3429e6adaca7a
Veronica Lourens <Veronica@lourens.cloud>
2022-04-14 11:26:54 +0000

    Migrations and admin.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
+
 ]
 
 MIDDLEWARE = [
@@ -62,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'todo'
+                
             ],
         },
     },
