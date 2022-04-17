866ecb12e93795f6deada90a1ce57aaefb319acd
Veronica Lourens <Veronica@lourens.cloud>
2022-04-17 08:08:33 +0000

    Update the development environment.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-m#g4z!bpywugnab-alr6%d34*c=k7geo#cs_%=bgc=sx2j6ua!')
 
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
